8bcb0e22e1c276303ec73e6699e67532574953ff
Az Bagh <azbiopep@gmail.com>
2020-05-08 23:52:40 -0400

    force pushing the tag

--- docs/tagging.sh
+++ docs/tagging.sh
@@ -7,6 +7,7 @@ tag_name="latest"
 commit_sha=$(git log -n1 --format=format:"%H")
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+create_tag(){
 command="curl -i \
 --request POST \
 -u username:$token \
@@ -15,3 +16,21 @@ https://api.github.com/repos/$owner/$repo/git/refs"
 
 git tag $tag_name
 eval "$command"
+}
+
+update_tag() {
+command="curl -i \
+--request PATCH \
+-u username:$token \
+--data '{\"sha\": \"$commit_sha\", \"force\":true}' \
+https://api.github.com/repos/$owner/$repo/git/refs/tags/$tag_name"
+
+eval "$command"
+}
+
+if git tag --list | egrep -q "$tag_name"
+then
+    update_tag
+else
+   create_tag
+fi
